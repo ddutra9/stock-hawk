6c2bc4b26cfa79c8aa3acbd7f6f03821167a750f
donato <ddutra9@gmail.com>
2017-11-09 13:16:22 -0200

    static widget sucess

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -1,11 +1,13 @@
 package com.udacity.stockhawk.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
+
 
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.ui.MainActivity;
@@ -20,7 +22,13 @@ public class StockWidgetProvider extends AppWidgetProvider {
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_stock_list);
 
+            Intent i = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, 0);
+            views.setOnClickPendingIntent(R.id.main_activity, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
